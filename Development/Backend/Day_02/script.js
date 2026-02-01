4ebca7f97878ff583762afa76c32e5f1194cb3fb
Satyam <kumarsatyam01303@gmail.com>
2026-02-01 20:54:55 +0530

    Add initial files for Day 02: example.txt, copied_file.txt, and script.js

---
+++ Development/Backend/Day_02/script.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// writefile
+// Append
+// copyfile
+// rename
+// unlink
+
+// Write a file
+// fs.writeFile('example.txt', 'Hello, World!', (err) => {
+//     if (err) throw err;
+//     console.log('File created successfully.');
+// });
+
+// Append to a file
+// fs.appendFile('example.txt', '\nAppended text.', (err) => {
+//     if (err) throw err;
+//     console.log('Text appended successfully.');
+// });
+
+// Copy a file
+fs.copyFile('example.txt', 'copied_file.txt', (err) => {
+    if (err) throw err;
+    console.log('File copied successfully.');
+});
