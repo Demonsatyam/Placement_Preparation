e8006ea2437550b9679933070c93d75d19926acd
Satyam <kumarsatyam01303@gmail.com>
2026-02-02 22:58:01 +0530

    installed packages for learning

---
+++ Development/Backend/Day_03/script.js
@@ -0,0 +1,15 @@
+// module - installed with core nodejs
+// packages - installed with npm
+
+// Role of NPM -> installing, uninstalling, managing packages
+
+// dependencies -> packages and packages ki dependencies
+
+// devdependencies -> packages required only during development
+
+// Ques: Kai baar ap koi script chalate ho and us script mein aap likhte ho yeh command
+// npm start
+// npm run dev
+// npm run concurrent
+
+console.log("Hello World");
