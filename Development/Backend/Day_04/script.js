cd8ccc671572ad2fec60155448758ff5ce7ba734
Satyam <kumarsatyam01303@gmail.com>
2026-02-03 14:17:17 +0530

    feat: add cookie-parser dependency and implement middleware for request logging

--- Development/Backend/Day_04/script.js
+++ Development/Backend/Day_04/script.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 
 // app middleware
-// app.use((req, res, next) => {
-//     console.log("middleware called");
-//     next();
-// });
+app.use((req, res, next) => {
+    console.log("middleware called");
+    next();
+});
 
-// routes
+// for json based data
+app.use(express.json());
+
+// x-www-form-urlencoded -> form data
+app.use(express.urlencoded({ extended: true }));
+
+routes
 app.get("/", (req, res, next) => {
     res.send('Welcome to ExpressJs Server');
 });
@@ -28,3 +34,14 @@ app.listen(3000);
 
 // Request and response handling
 // Frontend - Backend - Frontend
+
+// FORM HANDLING and WORKING with the FORMS
+// Handle backend process of forms and making sure the data coming from anu frontend lib, fw, templating engines
+// we still handle it at the backend
+
+// Install cookie-parser
+// cookie -> saving data at frontend and sending it to backend whenever a request is made
+// Hum log kuch bhi data frontend pe rkh skte hein and jb bhi aap kuch request backend pr kroge woh frontend pr saved data automatically backend pr chala jayega
+// Session -> store data at backend and whenever a request is made from frontend to backend, we identify the user using some unique id (cookie) and then fetch the data from backend using that id
+// Session basically stores the communication from LogIn to Logout
+
